b47df38f7a9b6ff4ecef4d1208e24b86641cd190
munro306 <154078717+munro306@users.noreply.github.com>
2025-03-04 18:41:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { google } = require('googleapis');
-const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,13 +7,15 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = 'token.json';
 
 app.get('/events', (req, res) => {
-    fs.readFile('credentials.json', (err, content) => {
-        if (err) return console.log('Error loading client secret file:', err);
-        authorize(JSON.parse(content), listEvents);
-    });
+    const credentials = {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uris: [process.env.REDIRECT_URI]
+    };
+    authorize(credentials, listEvents);
 
     function authorize(credentials, callback) {
-        const { client_secret, client_id, redirect_uris } = credentials.installed;
+        const { client_secret, client_id, redirect_uris } = credentials;
         const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris);
 
         fs.readFile(TOKEN_PATH, (err, token) => {
@@ -26,4 +27,13 @@ app.get('/events', (req, res) => {
 
     function getAccessToken(oAuth2Client, callback) {
         const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline
+            access_type: 'offline',
+            scope: SCOPES,
+        });
+        console.log('Authorize this app by visiting this url:', authUrl);
+    }
+
+    function listEvents(auth) {
+        const calendar = google.calendar({ version: 'v3', auth });
+        calendar.events.list({
+            calendarId: 'primary
